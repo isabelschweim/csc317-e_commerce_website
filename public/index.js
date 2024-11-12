467b547a13457329148deb236354ccd358e8a0c2
ThatCodebyTony <twu1415@gmail.com>
2024-11-11 18:15:20 -0800

    1. Created "back to top" button

--- public/index.js
+++ public/index.js
@@ -10,3 +10,20 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+// Get the button
+let backToTopBtn = document.getElementById("backToTopBtn");
+
+// When the user scrolls down 20px from the top, show the button
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+};
+
+// When the user clicks the button, scroll to the top of the document
+backToTopBtn.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' }); // Smooth scroll to top
+};
