d292e1b05b1cbba7333b972523cd1fc618c52049
ThatCodebyTony <twu1415@gmail.com>
2024-12-03 19:14:48 -0800

    Updated index.js products array with all NFTs

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,18 @@ const router = express.Router();
 // Route to render homepage
 router.get('/', (req, res) => {
   const products = [
-    { name: 'Purranosaurus', price: 4999.99, image: '4.jpg' },
-    { name: 'The Witness', price: 2999.99, image: '2.jpg' },
-    { name: 'Carnotadministrus', price: 1999.99, image: '3.jpg' }
+    { name: 'Carnotadministrus', price: 1999.99, image: 'Carnotadministrus.jpeg' },
+    { name: 'Chappy', price: 2999.99, image: 'Chappy.jpeg' },
+    { name: 'Chonkosaurus', price: 2499.99, image: 'Chonkosaurus.jpeg' },
+    { name: 'Creepclaw', price: 3599.99, image: 'Creepclaw.jpeg' },
+    { name: 'Eleganteryx', price: 4599.99, image: 'Eleganteryx.jpeg' },
+    { name: 'Gloomasaurus', price: 3999.99, image: 'Gloomasaurus.jpeg' },
+    { name: 'Johnny-Raptor', price: 3299.99, image: 'Johnny-Raptor.jpeg' },
+    { name: 'Karenodon', price: 2399.99, image: 'Karenodon.jpeg' },
+    { name: 'Purranosaurus', price: 4999.99, image: 'Purranosaurus.jpeg' },
+    { name: 'Rumoraptor', price: 2799.99, image: 'Rumoraptor.jpeg' },
+    { name: 'T-Wrecks', price: 3199.99, image: 'T-Wrecks.jpeg' },
+    { name: 'The Witness', price: 2999.99, image: 'The Witness.jpeg' }
   ];
   res.render('index', { products });
 });
