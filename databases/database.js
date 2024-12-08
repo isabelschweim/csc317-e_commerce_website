df5228347e1e0dff52c3213921a4face16c37c38
Steven Peralta <166183486+Stperalta7@users.noreply.github.com>
2024-12-08 14:04:11 -0800

    Update database.js

--- databases/database.js
+++ databases/database.js
@@ -2,7 +2,11 @@ const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database(':memory:')
 
 db.serialize(() => {
-  db.run('CREATE TABLE products (info TEXT)')
+  db.run('CREATE TABLE products (id INTEGER PRIMARY KEY,
+	                               title TEXT NOT NULL,
+	                               description TEXT NOT NULL,
+                              	 price REAL NOT NULL,
+	                               token TEXT NOT NULL)')
   const stmt = db.prepare('INSERT INTO products VALUES ( ... )')
 
   for (let i = 0; i < 10; i++) {
