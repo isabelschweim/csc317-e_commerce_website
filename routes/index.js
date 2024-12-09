d8c7c0589b1ff69a1e22db6ec1a6638e098e86ec
ThatCodebyTony <twu1415@gmail.com>
2024-12-08 18:03:02 -0800

    Refactor CSS: Add page-specific styles, improve mobile layout, and fix footer overflow 1. Gave each webpage its own CSS. 2. More mobile visual adjustments 3. fixed footer being wider than webpage

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const router = express.Router();
 // Route to render homepage
 router.get('/', (req, res) => {
   const products = [
-    { name: 'Carnotadministrus', price: 1999.99, image: 'Carnotadministrus.jpeg' },
-    { name: 'Chappy', price: 2999.99, image: 'Chappy.jpeg' },
+    { name: 'Carnotadministrus', price: 1999.99,description: "if other CSS selectors with higher specificity are targeting the .price element, they may override the font size. To ensure your font size rule is applied, you can increase the specificity of your selector by using a more specific one like:", image: 'Carnotadministrus.jpeg' },
+    { name: 'Chappy', price: 2999.99,description: "if other CSS selectors with higher specificity are targeting the .price element, they may", image: 'Chappy.jpeg' },
     { name: 'Chonkosaurus', price: 2499.99, image: 'Chonkosaurus.jpeg' },
     { name: 'Creepclaw', price: 3599.99, image: 'Creepclaw.jpeg' },
     { name: 'Eleganteryx', price: 4599.99, image: 'Eleganteryx.jpeg' },
